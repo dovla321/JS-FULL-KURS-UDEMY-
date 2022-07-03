e1b53ea25c030924b72beda9de9b184fa37b7e9f
dovla321 <dovla91mos@gmail.com>
2022-07-03 01:58:56 -1200

    Trenutno radim for loop

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -493,4 +493,56 @@ if(markMiller.bmiValue > johnSmith.bmiValue){
 	console.log(`${johnSmith.fullName} BMI is ${johnSmith.bmiValue} and it s heiger then  ${markMiller.fullName} BMI of ${markMiller.bmiValue}`);
 }
 
+
+//for loop keeps running while condition is true
+for(let rep = 1; rep<=10; rep ++){
+	console.log(`Lifting weights repetition ${rep}`)
+}
+
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const types = [];
+
+for(let i = 0; i < jonas.length; i++){
+	console.log(jonas[i],typeof jonas[i]);
+
+	//types[i] = typeof jonas[i];
+	types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+
+
+const years = [1991,2007,1969,2020];
+
+const ages =[];
+
+for(let i = 0; i < years.length; i++) {
+	ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+console.log('----ONLY STRINGS------')
+for(let i = 0; i < jonas.length; i++){
+	if(typeof jonas[i] !== 'string') continue;
+	console.log(jonas[i],typeof jonas[i]);
+
+}
+
+console.log('----BREAKS------')
+for(let i = 0; i < jonas.length; i++){
+	if(typeof jonas[i] === 'number') break;
+	console.log(jonas[i],typeof jonas[i]);
+
+}
 */
